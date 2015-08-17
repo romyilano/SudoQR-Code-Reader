60cd696613eaf91426d2ae15f74d8cb2a4b19655
Romy Ilano <romy@snowyla.com>
2015-08-17 15:41:54 -0700

    ta ta tum setting things up

--- SudoQR Code Obj-C/SudoQR Code Obj-C/SudoQRViewController.h
+++ SudoQR Code Obj-C/SudoQR Code Obj-C/SudoQRViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SudoQRViewController.h
 //  SudoQR Code Obj-C
 //
 //  Created by Romy on 8/17/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SudoQRViewController : UIViewController
 
 @end
-
---
+++ SudoQR Code Obj-C/SudoQR Code Obj-C/SudoQRViewController.m
@@ -0,0 +1,37 @@
+//
+//  SudoQRViewController.m
+//  SudoQR Code Obj-C
+//
+//  Created by Romy on 8/17/15.
+//  Copyright (c) 2015 Romy. All rights reserved.
+//
+
+#import "SudoQRViewController.h"
+
+@interface SudoQRViewController ()
+
+@end
+
+@implementation SudoQRViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
